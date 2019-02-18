0e98f648f7ff8ffca310fb654d90fb181686ce96
Pavel Andreyev <paaha98@gmail.com>
2019-02-18 22:32:42 +0300

    Start work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
 
-int main() {
-    
+int main(int argc, char** argv){
+
+
     return 0;
 }
