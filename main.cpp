7c6c98bb60a30eb01a425c13baa71685588b79ab
Pavel Andreyev <paaha98@gmail.com>
2019-03-02 11:36:27 +0300

    Уменьшено количество семафоров

--- main.cpp
+++ main.cpp
@@ -13,11 +13,7 @@
 using namespace std;
 
 #define QUEUE 2000
-#define ROM_76_1_SEM 2001
-#define ROM_76_2_SEM 2002
-#define ROM_92_1_SEM 2003
-#define ROM_92_2_SEM 2004
-#define ROM_95_1_SEM 2005
+#define SEM 2001
 
 #define PERMS 0666
 
@@ -38,10 +34,34 @@ void sys_err(const char *);
 unsigned int serviceTime;
 unsigned int period;
 unsigned int maxSize;
+bool flowClosed;
+bool queueIsEmpty;
 
 int main(int argc, char **argv) {
     init(argc, argv);
     cout << serviceTime << " " << period << " " << maxSize << endl;
+
+    pthread_t queue;
+    int result;
+
+    result = pthread_create(&queue, nullptr, start_cars_flow, nullptr);
+    if (result != 0) {
+        perror("first thread creating");
+        return EXIT_FAILURE;
+    }
+
+    result = pthread_join(queue, nullptr);
+    if (result != 0) {
+        perror("waiting for a first thread");
+        return EXIT_FAILURE;
+    }
+
+    int semid;
+
+    if ((semid = semget(SEM, 1, PERMS | IPC_CREAT)) < 0) {
+        sys_err("terminal : can not get semaphore");
+    }
+
 
     return 0;
 }
@@ -72,81 +92,98 @@ void init(int argc, char **argv) {
 }
 
 void *start_terminal(void *arg) {
-    const int SEM_ID = *(int *) arg;
-    int fuel_type = 3; //TODO
+    const int fuel_type = *(int *) arg;
     int semid, shmid;
-    Car *myCar;
-    Car **cars;
+    Car *cars;
 
-    if ((semid = semget(SEM_ID, 1, PERMS | IPC_CREAT)) < 0) {
+    if ((semid = semget(SEM, 1, 0)) < 0) {
         sys_err("terminal : can not get semaphore");
     }
-    if ((shmid = shmget(QUEUE, sizeof(Car) * maxSize, PERMS | IPC_CREAT)) < 0) {
+    if ((shmid = shmget(QUEUE, sizeof(Car) * maxSize, 0)) < 0) {
         sys_err("server: can not get shared memory segment");
     }
-    if ((cars = (Car **) shmat(shmid, nullptr, 0)) == nullptr) {
+    if ((cars = (Car *) shmat(shmid, nullptr, 0)) == nullptr) {
         sys_err("server: shared memory attach error");
     }
 
+
     semctl(semid, 0, SETVAL, 0);
+
     while (true) {
+        label:
         if (semctl(semid, 0, GETVAL, 0))
             continue;
 
         semctl(semid, 0, SETVAL, 1);
 
         for (int i = 0; i < maxSize; i++) {
-            myCar = cars[i];
-            if (myCar->fuel_type == fuel_type) {
-
+            if (cars[i].fuel_type == fuel_type) {
+                cars[i] = nullptr;//TODO:
                 break;
             }
         }
 
+        semctl(semid, 0, SETVAL, 0);
 
         sleep(serviceTime);
 
+        if (flowClosed) {
+            for (int i = 0; i < maxSize; i++) {
+                if (cars[i] != nullptr) {
+                    goto label;
+                }
+            }
 
-        semctl(semid, 0, SETVAL, 0);
-        if (true) break;
-    }
-
-    if (semctl(semid, 0, IPC_RMID, (struct semid_ds *) 0) < 0) {
-        sys_err("server: semaphore remove error");
+            if (!queueIsEmpty && shmctl(shmid, IPC_RMID, (struct shmid_ds *) nullptr) < 0) {
+                queueIsEmpty = true;
+                sys_err("server: shared memory remove error");
+            }
+            break;
+        }
     }
 
     shmdt(cars);
-    if (shmctl(shmid, IPC_RMID, (struct shmid_ds *) nullptr) < 0) {
-        sys_err("server: shared memory remove error");
-    }
+
     exit(0);
 }
 
 void *start_cars_flow(void *arg) {
 
-    int shmid;
-    Car **cars;
+    printf("start cars flow\n");
 
-    if ((shmid = shmget(QUEUE, sizeof(Car) * maxSize, 0)) < 0) {
+    int shmid, semid;
+    Car *cars; //TODO:
+
+    if ((semid = semget(SEM, 1, 0)) < 0) {
+        sys_err("terminal : can not get semaphore");
+    }
+    if ((shmid = shmget(QUEUE, sizeof(Car) * maxSize, PERMS | IPC_CREAT)) < 0) {
         sys_err("client: can not get shared memory segment");
     }
 
-    if ((cars = (Car**) shmat(shmid, nullptr, 0)) == nullptr) {
+    if ((cars = (Car *) shmat(shmid, nullptr, 0)) == nullptr) {
         sys_err("client: shared memory attach error");
     }
 
+    Car *c = new(cars) Car[maxSize];
+
     for (int i = 0; i < 150; i++) {
+        while (semctl(semid, 0, GETVAL, 0));
+        semctl(semid, 0, SETVAL, 1);
         Car car;
         car.car_id = i;
         car.fuel_type = rand() % 4;
-        for(int j = 0; j < maxSize; j++){
-            if(cars[j] == nullptr){
-                cars[j] = &car;
+        for (int j = 0; j < maxSize; j++) {
+            if (cars[j] == nullptr) {
+                cars[j] = &car; //TODO:
+                printf("car #%d with fuel type #%d is gas station", cars[j]->car_id, cars[j]->fuel_type);
                 break;
             }
         }
+        semctl(semid, 0, SETVAL, 0);
         sleep(period);
     }
+    flowClosed = true;
     shmdt(cars);
     exit(0);
 }
